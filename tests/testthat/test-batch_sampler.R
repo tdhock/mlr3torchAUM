e6dae11ea237c0e84aa9694787e5599e71a7af57
caowei <weicao92@163.com>
2026-03-06 23:36:33 +0800

    adjust batch_list_list position

--- tests/testthat/test-batch_sampler.R
+++ tests/testthat/test-batch_sampler.R
@@ -126,8 +126,8 @@ test_that("stratified sampler: shuffle, unit test", {
   Class_vec <- sonar_task$data(sonar_task$row_ids, "Class")$Class
   batch_sampler_class <- mlr3torchAUM::batch_sampler_stratified(
     min_samples_per_stratum = 1, shuffle = TRUE)
-  batch_list_list <- list()
   batch_sampler_instance <- batch_sampler_class(list(task = sonar_task))
+  batch_list_list <- list()
   for(rep in 1:2){
     if(torch::torch_is_installed()){
       torch::torch_manual_seed(888)
