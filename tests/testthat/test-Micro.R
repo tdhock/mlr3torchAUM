8020de37370cd29079f9cb226154b9c4b368164c
caowei <weicao92@163.com>
2026-06-05 14:48:57 -0400

    fix further test macro in Micro

--- tests/testthat/test-Micro.R
+++ tests/testthat/test-Micro.R
@@ -1,7 +1,7 @@
 test_that("micro_auc: basic properties and small known cases", {
   skip_on_cran()
 
-  ##  Case 1: perfect 3-class separation  macro AUC = 1
+  ##  Case 1: perfect 3-class separation  micro AUC = 1
   labels <- torch::torch_tensor(c(1L, 2L, 3L, 1L, 2L, 3L), dtype = torch::torch_long())
   P <- matrix(
     c(
@@ -16,7 +16,7 @@ test_that("micro_auc: basic properties and small known cases", {
   )
   pred <- torch::torch_tensor(P, dtype = torch::torch_float())
 
-  auc_pkg <- ROC_AUC_macro(pred, labels)$item()
+  auc_pkg <- ROC_AUC_micro(pred, labels)$item()
 
   expect_true(is.numeric(auc_pkg))
   expect_equal(auc_pkg, 1.0, tolerance = 1e-6)
@@ -27,17 +27,17 @@ test_that("micro_auc: basic properties and small known cases", {
   labels2 <- torch::torch_tensor(sample.int(K, N, replace = TRUE) , dtype = torch::torch_long())
   logits2 <- torch::torch_randn(c(N, K))
   probs2 <- torch::nnf_softmax(logits2, dim = 2)
-  auc_pkg2 <- ROC_AUC_macro(probs2, labels2)$item()
+  auc_pkg2 <- ROC_AUC_micro(probs2, labels2)$item()
 
   expect_true(auc_pkg2 >= 0 && auc_pkg2 <= 1)
   # Random baseline should be ~0.5; allow some slack with many classes.
   expect_true(abs(auc_pkg2 - 0.5) < 0.1)
 
 })
-test_that("macro_aum: basic properties and small known cases", {
+test_that("micro_aum: basic properties and small known cases", {
   skip_on_cran()
 
-  ##  Case 1: perfect 3-class separation  macro AUC = 1
+  ##  Case 1: perfect 3-class separation  micro AUC = 1
   labels <- torch::torch_tensor(c(1L, 2L, 3L, 1L, 2L, 3L), dtype = torch::torch_long())
   P <- matrix(
     c(
