a562eb81a2aac173213fef6200f33984e5f09872
caowei <weicao92@163.com>
2026-06-14 08:53:27 -0400

    final version of map_class_lables

--- tests/testthat/test-imcp.R
+++ tests/testthat/test-imcp.R
@@ -35,3 +35,28 @@ test_that("map_class_labels: in normal case, map labels to 1..k", {
   expect_equal(res2$y_true_size, 3)
   expect_equal(res2$y_true_int_encoded, c(2, 2, 1, 3))
 })
+
+test_that("map_class_labels: check when cols of y_score is not equal to the number of classes", {
+  # y_true only has 2 classes but y_score has 3, must provide labels
+  y_true <- c(1, 1, 2, 2)
+  y_score <- matrix(1/3, nrow = 4, ncol = 3)
+
+  # 1. no labels provided
+  expect_error(map_class_labels(y_true, y_score), "not given")
+  # 2. labels length is not right
+  expect_error(map_class_labels(y_true, y_score, labels = c(1, 2)), "not equal")
+  expect_error(map_class_labels(y_true, y_score, labels = c(1, 2, 3, 4)), "not equal")
+  # 3. types are not corret: numeric y_true with characters labels
+  expect_error(map_class_labels(y_true, y_score, labels = c(1, 2, "d")), "type does not match")
+  # 4. y_true classes are not a subset of labels (missing 2)
+  expect_error(map_class_labels(y_true, y_score, labels = c(1, 3, 4)), "subset")
+  # 5. provide all c(1,2,3), should succeed
+  res <- map_class_labels(y_true, y_score, labels = c(1, 2, 3))
+  expect_equal(res$y_true_size, 3)
+  expect_equal(res$y_true_int_encoded, c(1, 1, 2, 2))
+
+  # 6. labels in random order should also get the same result (because internal sort)
+  res_shuffled <- map_class_labels(y_true, y_score, labels = c(3, 1, 2))
+  expect_equal(res_shuffled$y_true_size, 3)
+  expect_equal(res_shuffled$y_true_int_encoded, c(1, 1, 2, 2))
+})
