eb43674c834fa667d5c061081fa19a086e5abdfc
caowei <weicao92@163.com>
2026-06-18 09:15:04 -0400

    negative term and positve term + tests

--- R/AUCM.R
+++ R/AUCM.R
@@ -1,10 +1,15 @@
-AUCM <- function(pred_tensor, label_tensor) {
-  device <- pred_tensor$device
-  return(torch::torch_zeros(1, device = device))
+AUCM <- function(pred_tensor, label_tensor, a = 0, b = 0) {
+  p <- positive_ratio(label_tensor)
+  N <- label_tensor$numel()
+  pos_mask <- (label_tensor$flatten() == 1L)$to(torch::torch_float())
+  neg_mask <- 1 - pos_mask
+  pos_term <- (1 - p) * ((pred_tensor$flatten() - a)^2 * pos_mask)$sum() / N
+  neg_term <- p * ((pred_tensor$flatten() - b)^2 * neg_mask)$sum() / N
+  return(pos_term + neg_term)
 }
 
 positive_ratio <- function(label_tensor) {
   label_tensor <- label_tensor$flatten()
   label_tensor_bool <- label_tensor == 1L
-  return(torch::torch_mean(label_tensor_bool$to(torch::torch_float())))
+  return(label_tensor_bool$to(torch::torch_float())$mean())
 }
