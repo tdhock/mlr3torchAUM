795c71bbf9e130c2cc18d64af569f7b81a1ede23
caowei <weicao92@163.com>
2026-06-18 06:28:33 -0400

    postive_ratio helper function

--- tests/testthat/test-AUCM.R
+++ tests/testthat/test-AUCM.R
@@ -1,11 +1,16 @@
 if (torch::torch_is_installed() && requireNamespace("mlr3torch")) {
-
-  test_that("Step1: AUCMLoss_value returns 0", {
+  test_that("Step1: AUCM returns 0", {
     skip_on_cran()
     pred <- torch::torch_tensor(c(0.5))
     label <- torch::torch_tensor(c(1))
-    out <- AUCMLoss(pred, label)
+    out <- AUCM(pred, label)
     expect_equal(out$item(), 0, tolerance = 1e-6)
+  })
+
+  test_that("Step2: positive_ratio returns the ratio of positives", {
+    skip_on_cran()
+    expect_equal(positive_ratio(torch::torch_tensor(c(0, 0, 1, 1)))$item(), 0.5, tolerance = 1e-6)
+    expect_equal(positive_ratio(torch::torch_tensor(c(0, 1, 1, 1)))$item(), 0.75, tolerance = 1e-6)
   })
 
 }
