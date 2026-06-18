9e5f84b6be27646b465c830ce1cfe48a3368d7e0
caowei <weicao92@163.com>
2026-06-18 10:45:31 -0400

    complete v1

--- R/AUCM.R
+++ R/AUCM.R
@@ -1,11 +1,14 @@
-AUCM <- function(pred_tensor, label_tensor, a = 0, b = 0) {
+AUCM <- function(pred_tensor, label_tensor, a = 0, b = 0, alpha = 0, margin = 1) {
   p <- positive_ratio(label_tensor)
   N <- label_tensor$numel()
   pos_mask <- (label_tensor$flatten() == 1L)$to(torch::torch_float())
   neg_mask <- 1 - pos_mask
-  pos_term <- (1 - p) * ((pred_tensor$flatten() - a)^2 * pos_mask)$sum() / N
-  neg_term <- p * ((pred_tensor$flatten() - b)^2 * neg_mask)$sum() / N
-  return(pos_term + neg_term)
+  s <- pred_tensor$flatten()
+  pos_term <- (1 - p) * ((s - a)^2 * pos_mask)$sum() / N
+  neg_term <- p * ((s - b)^2 * neg_mask)$sum() / N
+  cross <- 2 * alpha * (p * (1 - p) * margin + (p * s * neg_mask - (1 - p) * s * pos_mask)$sum() / N)
+  dual <- p * (1 - p) * alpha^2
+  return(pos_term + neg_term + cross - dual)
 }
 
 positive_ratio <- function(label_tensor) {
