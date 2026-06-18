ba5a9e8b9925eb44d184e83975298ba9a7a342a6
caowei <weicao92@163.com>
2026-06-18 06:28:33 -0400

    postive_ratio helper function

--- R/AUCM.R
+++ R/AUCM.R
@@ -1,4 +1,10 @@
-AUCMLoss <- function(pred_tensor, label_tensor) {
-    device <- pred_tensor$device
-    return(torch::torch_zeros(1, device=device))
+AUCM <- function(pred_tensor, label_tensor) {
+  device <- pred_tensor$device
+  return(torch::torch_zeros(1, device = device))
+}
+
+positive_ratio <- function(label_tensor) {
+  label_tensor <- label_tensor$flatten()
+  label_tensor_bool <- label_tensor == 1L
+  return(torch::torch_mean(label_tensor_bool$to(torch::torch_float())))
 }
