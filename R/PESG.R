db5a01dd289bce09f35466cf157fc978e3ad3d0b
caowei <weicao92@163.com>
2026-06-20 05:41:10 -0400

    fix alpha update error

--- R/PESG.R
+++ R/PESG.R
@@ -9,3 +9,20 @@ pesg_alpha_step <- function(loss_module, lr) {
   })
   invisible(loss_module)
 }
+
+pesg_step <- function(loss_module, lr) {
+  torch::with_no_grad({
+    a <- loss_module$a
+    b <- loss_module$b
+    alpha <- loss_module$alpha
+    a$sub_(lr * a$grad)
+    b$sub_(lr * b$grad)
+  })
+  pesg_alpha_step(loss_module, lr)
+  torch::with_no_grad({
+    a$grad$zero_()
+    b$grad$zero_()
+    alpha$grad$zero_()
+  })
+  invisible(loss_module)
+}
