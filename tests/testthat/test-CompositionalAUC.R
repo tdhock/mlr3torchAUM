a983840143553b02ed549d62c4c8759735bbf2f8
caowei <weicao92@163.com>
2026-06-20 23:18:37 -0400

    inital commit: tdd for is_ce_step

---
+++ tests/testthat/test-CompositionalAUC.R
@@ -0,0 +1,16 @@
+# Tests for CompositionalAUCLoss + PDSCA (LibAUC port)
+
+test_that("is_ce_step alternates CE/AUCM on the 2k schedule", {
+  # step %% (2*k) < k is TRUE -> CE branch, else AUCM
+  # k=1: CE, AUCM, CE, AUCM, ...
+  expect_true (is_ce_step(0L, 1L))
+  expect_false(is_ce_step(1L, 1L))
+  expect_true (is_ce_step(2L, 1L))
+  expect_false(is_ce_step(3L, 1L))
+  # k=2: CE, CE, AUCM, AUCM, ...
+  expect_true (is_ce_step(0L, 2L))
+  expect_true (is_ce_step(1L, 2L))
+  expect_false(is_ce_step(2L, 2L))
+  expect_false(is_ce_step(3L, 2L))
+})
+
