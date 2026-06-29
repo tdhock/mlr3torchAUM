b76d32de3016ee5b51ba151e1525b1fb2087c2ec
caowei <weicao92@163.com>
2026-06-28 20:33:48 -0400

    construct nn_CompositionalAUC_loss initialize

--- R/CompositionalAUC.R
+++ R/CompositionalAUC.R
@@ -1,3 +1,14 @@
 is_ce_step <- function(step, k) {
   return(step %% (2 * k) < k)
 }
+
+nn_CompositionalAUC_loss <- torch::nn_module(
+  c("nn_CompositionalAUC_loss", "nn_loss"),
+  initialize = function(margin = 1, k = 1) {
+    self$a <- torch::nn_parameter(torch::torch_zeros(1))
+    self$b <- torch::nn_parameter(torch::torch_zeros(1))
+    self$alpha <- torch::nn_parameter(torch::torch_zeros(1))
+    self$margin <- margin
+    self$k <- 1
+  }
+)
