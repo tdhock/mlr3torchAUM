abd661a3a8f69272acdc47531cad6a1fdd1a29cd
caowei <weicao92@163.com>
2026-06-29 06:05:57 -0400

    complete ce

--- R/CompositionalAUC.R
+++ R/CompositionalAUC.R
@@ -8,7 +8,16 @@ nn_CompositionalAUC_loss <- torch::nn_module(
     self$a <- torch::nn_parameter(torch::torch_zeros(1))
     self$b <- torch::nn_parameter(torch::torch_zeros(1))
     self$alpha <- torch::nn_parameter(torch::torch_zeros(1))
+    self$step <- torch::nn_buffer(torch::torch_zeros(1))
     self$margin <- margin
     self$k <- k
+  },
+  forward = function(pred, target) {
+    if (is_ce_step(self$step$item(), self$k)) {
+      self$step$add_(1L)
+      return(torch::nnf_binary_cross_entropy(
+        pred$flatten(), target$flatten()
+      ))
+    }
   }
 )
