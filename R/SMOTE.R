ec31e1a9b08e89712a64657e43709c2e1c6365bc
caowei <weicao92@163.com>
2026-06-30 10:01:21 -0400

    delete unnecessary assert

--- R/SMOTE.R
+++ R/SMOTE.R
@@ -4,12 +4,16 @@ count_class <- function(y) {
 }
 
 check_sampling_strategy <- function(y, strategy = "auto") {
+  count <- count_class(y)
+  max_value <- max(count)
   if (strategy == "auto") {
-    count <- count_class(y)
-    max_value <- max(count)
     major_name <- names(which.max(count))[[1]]
     other_count <- count[names(count) != major_name]
     return(max_value - other_count)
   }
-  stop("not implemented")
+  if (strategy == "minority") {
+    min_names <- names(which.min(count))
+    min_count <- count[names(count) == min_names]
+    return(max_value - min_count)
+  }
 }
--- tests/testthat/test-SMOTE.R
+++ tests/testthat/test-SMOTE.R
@@ -12,4 +12,19 @@ test_that("test auto mode of check_sampling strategy", {
     strategy = "auto"
   )
   expect_identical(samp_strategy, c("0" = 800L))
+  samp_strategy_multi <- check_sampling_strategy(
+    factor(c(rep("0", 50), rep("1", 100), rep("2", 1000))),
+    strategy = "auto"
+  )
+  expect_identical(samp_strategy_multi, c("0" = 950L, "1" = 900L))
+  samp_strategy_multi_mino <- check_sampling_strategy(
+    factor(c(rep("0", 50), rep("1", 100), rep("2", 1000))),
+    strategy = "minority"
+  )
+  expect_identical(samp_strategy_multi_mino, c("0" = 950L))
+  samp_strategy_multi_mino2 <- check_sampling_strategy(
+    factor(c(rep("0", 50), rep("1", 50), rep("2", 1000))),
+    strategy = "minority"
+  )
+  expect_identical(samp_strategy_multi_mino2, c("0" = 950L)) # get just one
 })
