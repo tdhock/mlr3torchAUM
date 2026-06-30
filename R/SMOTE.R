24c60f218ac417c94b6065ede2874c2470fa9929
caowei <weicao92@163.com>
2026-06-30 09:37:03 -0400

    add auto mode of check_sampling_strategy

--- R/SMOTE.R
+++ R/SMOTE.R
@@ -2,3 +2,14 @@ count_class <- function(y) {
   table_y <- table(y)
   return(setNames(as.integer(table_y), names(table_y)))
 }
+
+check_sampling_strategy <- function(y, strategy = "auto") {
+  if (strategy == "auto") {
+    count <- count_class(y)
+    max_value <- max(count)
+    major_name <- names(which.max(count))[[1]]
+    other_count <- count[names(count) != major_name]
+    return(max_value - other_count)
+  }
+  stop("not implemented")
+}
--- tests/testthat/test-SMOTE.R
+++ tests/testthat/test-SMOTE.R
@@ -5,3 +5,11 @@ test_that("test helper function count class", {
   count_result <- count_class(factor(c(rep("0", 100), rep("1", 900))))
   expect_identical(count_result, c("0" = 100L, "1" = 900L))
 })
+
+test_that("test auto mode of check_sampling strategy", {
+  samp_strategy <- check_sampling_strategy(
+    factor(c(rep("0", 100), rep("1", 900))),
+    strategy = "auto"
+  )
+  expect_identical(samp_strategy, c("0" = 800L))
+})
