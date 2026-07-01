4ddb13857761d5e73e357f4524014be39c5fdeb0
caowei <weicao92@163.com>
2026-07-01 19:09:21 -0400

    refactor fitsample by lapply)

--- R/SMOTE.R
+++ R/SMOTE.R
@@ -46,19 +46,19 @@ fit_resample <- function(X, y, strategy = "auto", k = 5) {
   n_samples <- nrow(X)
   if (n_samples != length(y)) stop("data and label not consistent")
   sampling_strategy <- check_sampling_strategy(y, strategy)
-  X_pieces <- list(X)
-  y_pieces <- list(as.character(y))
-  for (class_name in names(sampling_strategy)) {
+  results <- lapply(names(sampling_strategy), function(class_name) {
     n_to_generate <- sampling_strategy[[class_name]]
-    if (n_to_generate == 0) next
-    X_within_class <- X[as.character(y) == class_name, , drop = FALSE]
-    nn <- knn_within_class(X_within_class, k)
-    generated <- make_samples(X_within_class, nn, n_to_generate)
-    X_pieces[[length(X_pieces) + 1]] <- generated
-    y_pieces[[length(y_pieces) + 1]] <- rep(class_name, n_to_generate)
-  }
+    if (n_to_generate != 0) {
+      X_within_class <- X[as.character(y) == class_name, , drop = FALSE]
+      nn <- knn_within_class(X_within_class, k)
+      generated <- make_samples(X_within_class, nn, n_to_generate)
+      return(list(X = generated, y = rep(class_name, n_to_generate)))
+    }
+  })
   return(list(
-    X = do.call(rbind, X_pieces),
-    y = factor(unlist(y_pieces), levels = levels(y))
+    X = do.call(rbind, c(list(X), lapply(results, function(result) result$X))),
+    y = factor(c(as.character(y), unlist(lapply(results, function(result) result$y))),
+      levels = levels(y)
+    )
   ))
 }
