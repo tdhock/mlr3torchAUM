e648bc20a23cdda0d563c3b6c74391e175aa5241
caowei <weicao92@163.com>
2026-07-01 19:11:29 -0400

    refactor fitsample by lapply 2

--- R/SMOTE.R
+++ R/SMOTE.R
@@ -46,14 +46,13 @@ fit_resample <- function(X, y, strategy = "auto", k = 5) {
   n_samples <- nrow(X)
   if (n_samples != length(y)) stop("data and label not consistent")
   sampling_strategy <- check_sampling_strategy(y, strategy)
+  sampling_strategy <- sampling_strategy[sampling_strategy > 0]
   results <- lapply(names(sampling_strategy), function(class_name) {
     n_to_generate <- sampling_strategy[[class_name]]
-    if (n_to_generate != 0) {
-      X_within_class <- X[as.character(y) == class_name, , drop = FALSE]
-      nn <- knn_within_class(X_within_class, k)
-      generated <- make_samples(X_within_class, nn, n_to_generate)
-      return(list(X = generated, y = rep(class_name, n_to_generate)))
-    }
+    X_within_class <- X[as.character(y) == class_name, , drop = FALSE]
+    nn <- knn_within_class(X_within_class, k)
+    generated <- make_samples(X_within_class, nn, n_to_generate)
+    return(list(X = generated, y = rep(class_name, n_to_generate)))
   })
   return(list(
     X = do.call(rbind, c(list(X), lapply(results, function(result) result$X))),
