215d283878bf7c40f862d3c0129f33b88d0f160b
caowei <weicao92@163.com>
2026-07-02 08:21:08 -0400

    add n_data for generate_samplers helper function for borderline SMOTE

--- R/SMOTE.R
+++ R/SMOTE.R
@@ -24,14 +24,14 @@ knn_within_class <- function(X, k) {
   return(RANN::nn2(X, X, k = k + 1)$nn.idx[, -1, drop = FALSE])
 }
 
-generate_samples <- function(X, nn, rows, cols, steps) {
+generate_samples <- function(X, nn_num, rows, cols, steps, nn_data = X) {
   num_neigh <- length(rows)
   if (num_neigh != length(cols) || num_neigh != length(steps)) stop("rows, cols and steps not consistent")
   if (any(rows > nrow(X))) stop("illegal row number")
-  if (any(cols > ncol(nn))) stop("illegal col number")
+  if (any(cols > ncol(nn_num))) stop("illegal col number")
   if (any(steps > 1 | steps < 0)) stop("illegal step")
   x <- X[rows, , drop = FALSE]
-  neighs <- X[nn[cbind(rows, cols)], , drop = FALSE]
+  neighs <- nn_data[nn_num[cbind(rows, cols)], , drop = FALSE]
   return(x + steps * (neighs - x))
 }
 
