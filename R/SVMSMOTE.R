53e151b871e6eccf3a5c4224fee56802acf9f1f5
caowei <weicao92@163.com>
2026-07-04 08:24:34 -0400

    add fit_svm

--- R/SVMSMOTE.R
+++ R/SVMSMOTE.R
@@ -1,3 +1,9 @@
 minority_support_vectors <- function(svm_index, y, target_class) {
   return(svm_index[as.character(y[svm_index]) == target_class])
 }
+
+fit_svm <- function(X, y) {
+  if (!requireNamespace("e1071")) stop("e1071 not installed ")
+  gamma <- 1 / (ncol(X) * (mean(X^2) - mean(X)^2))
+  e1071::svm(x = X, y = y, kernel = "radial", cost = 1, gamma = gamma, scale = FALSE)
+}
--- tests/testthat/test-SVMSMOTE.R
+++ tests/testthat/test-SVMSMOTE.R
@@ -6,3 +6,15 @@ test_that("test helper function minority_support_vectors", {
   target_class <- "min"
   expect_identical(minority_support_vectors(svm_res$index, y, target_class), c(2L, 3L, 8L))
 })
+
+test_that("fit_svm returns SV index", {
+  skip_if_not_installed("e1071")
+  set.seed(1)
+  X <- matrix(rnorm(40), nrow = 20, ncol = 2)
+  y <- factor(rep(c("min", "maj"), c(6, 14)))
+  m1 <- fit_svm(X, y)
+  expect_true(length(m1$index) > 0)
+  set.seed(1)
+  m2 <- fit_svm(X, y)
+  expect_identical(m1$index, m2$index)
+})
