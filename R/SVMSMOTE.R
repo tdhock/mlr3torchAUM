9548c97a2f1c0728c46375e5ef9052f7d0bb45ec
caowei <weicao92@163.com>
2026-07-04 18:22:24 -0400

    clean up the guards

--- R/SVMSMOTE.R
+++ R/SVMSMOTE.R
@@ -73,24 +73,18 @@ SVMSMOTE <- R6::R6Class(
           n_gen_danger <- as.integer(frac * (n_to_generate + 1))
           n_gen_safe <- n_to_generate - n_gen_danger
         }
-        generated_danger <- X[integer(0), , drop = FALSE]
-        generated_safe <- X[integer(0), , drop = FALSE]
-        if (n_gen_danger > 0L) {
-          generated_danger <- private$.make_samples(
-            X[danger_svs, , drop = FALSE],
-            self$nn_k_(query = X[danger_svs, , drop = FALSE], data = X_within_class),
-            n_gen_danger,
-            nn_data = X_within_class
-          )
-        }
-        if (n_gen_safe > 0L) {
-          generated_safe <- private$.make_samples(
-            X[safe_svs, , drop = FALSE],
-            self$nn_k_(query = X[safe_svs, , drop = FALSE], data = X_within_class),
-            n_gen_safe,
-            nn_data = X_within_class, step_size = -self$out_step
-          )
-        }
+        generated_danger <- private$.make_samples(
+          X[danger_svs, , drop = FALSE],
+          self$nn_k_(query = X[danger_svs, , drop = FALSE], data = X_within_class),
+          n_gen_danger,
+          nn_data = X_within_class
+        )
+        generated_safe <- private$.make_samples(
+          X[safe_svs, , drop = FALSE],
+          self$nn_k_(query = X[safe_svs, , drop = FALSE], data = X_within_class),
+          n_gen_safe,
+          nn_data = X_within_class, step_size = -self$out_step
+        )
         generated <- rbind(generated_danger, generated_safe)
         return(list(X = generated, y = rep(class_name, nrow(generated))))
       })
