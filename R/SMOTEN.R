33d0d5cd380a2d9c44dd92e86dab0ad33ac8f92b
caowei <weicao92@163.com>
2026-07-05 08:01:12 -0400

    finish make_samples_nominal

--- R/SMOTEN.R
+++ R/SMOTEN.R
@@ -4,3 +4,20 @@ get_feature_wise_mode <- function(neighbors) {
     names(counts)[which.max(counts)]
   }))
 }
+
+make_samples_nominal <- function(X, nn_indices, n_to_generate) {
+  # n_to_generate > 0
+  n_samples <- nrow(X)
+  if (n_samples == 0) {
+    return(matrix(character(0), ncol = ncol(X)))
+  }
+  if (n_samples != nrow(nn_indices)) stop("sample number not consistent")
+  sample_indices <- sample.int(n_samples, n_to_generate, replace = TRUE)
+  return(t(sapply(
+    sample_indices,
+    function(sample_idx) {
+      neighbors <- X[nn_indices[sample_idx, ], , drop = FALSE]
+      return(get_feature_wise_mode(neighbors))
+    }
+  )))
+}
--- tests/testthat/test-SMOTEN.R
+++ tests/testthat/test-SMOTEN.R
@@ -16,3 +16,31 @@ test_that("test get_feature_wise_mode", {
     c("red", "sweet")
   )
 })
+
+test_that("test make_samples_nominal", {
+  set.seed(1)
+  X <- matrix(c(
+    "red", "sweet", "red", "salty",
+    "blue", "salty", "blue", "sweet"
+  ), nrow = 4, byrow = TRUE)
+  nn_indices <- matrix(c(2, 4, 1, 3, 2, 4, 1, 3), ncol = 2, byrow = TRUE)
+  samples1 <- make_samples_nominal(X, nn_indices, 3)
+  expect_equal(dim(samples1), c(3, ncol(X))) # n_to_generate * num_features
+  set.seed(1)
+  samples2 <- make_samples_nominal(X, nn_indices, 3)
+  expect_identical(samples1, samples2)
+  # edge case: only one sample
+  X_one_sample <- matrix(c("red", "sweet"), nrow = 1)
+  nn_indices_one_sample <- matrix(1, nrow = 1)
+  samples4 <- make_samples_nominal(X_one_sample, nn_indices_one_sample, 3)
+  expect_equal(nrow(unique(samples4)), 1L)
+  expect_equal(samples4[1, ], X_one_sample[1, ])
+  expect_equal(samples4, X_one_sample[rep(1, 3), , drop = FALSE])
+  # edge case: no samples
+  X_empty <- matrix(character(0), ncol = 2) # num_features = 2
+  nn_indices_empty <- matrix(integer(0), ncol = 2) # "k" of knn is 2
+  samples5 <- make_samples_nominal(X_empty, nn_indices_empty, 3)
+  expect_identical(samples5, matrix(character(0), ncol = 2))
+  # edge case: nn_neighbors rows not consistent
+  expect_error(make_samples_nominal(X, nn_indices_one_sample, 3), "not consistent")
+})
