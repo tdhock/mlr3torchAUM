0d0ca61e492a5d44c36659ce38e42f7aec2a2e40
caowei <weicao92@163.com>
2026-08-03 07:57:01 -0400

    fix! wrap pesg_primal_step in with_no_grad

--- R/PESG.R
+++ R/PESG.R
@@ -17,16 +17,17 @@ pesg_buffer_momentum <- function(dp, buffer, momentum) {
 pesg_primal_step <- function(p, grad,
                              lr, clamp_value, weight_decay,
                              epoch_decay, model_ref, momentum, buffer, model_acc) {
-  dp <- pesg_d_p(grad, p, clamp_value, weight_decay, epoch_decay, model_ref)
-  if (is.null(buffer)) {
-    buffer_new <- dp$clone()
-  } else {
-    buffer_new <- pesg_buffer_momentum(dp, buffer, momentum)
-  }
   torch::with_no_grad({
+    dp <- pesg_d_p(grad, p, clamp_value, weight_decay, epoch_decay, model_ref)
+    if (is.null(buffer)) {
+      buffer_new <- dp$clone()
+    } else {
+      buffer_new <- pesg_buffer_momentum(dp, buffer, momentum)
+    }
     p$sub_(lr * buffer_new)
+    res <- list(buffer = buffer_new, model_acc = model_acc + p)
   })
-  return(list(buffer = buffer_new, model_acc = model_acc + p))
+  return(res)
 }
 
 pesg_update_regularizer <- function(model_acc, T) {
