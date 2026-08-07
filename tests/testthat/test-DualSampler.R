867821562a9eb75ce5952bba973e3696f269c629
caowei <weicao92@163.com>
2026-08-07 16:08:37 -0400

    dual_class_indices

--- tests/testthat/test-DualSampler.R
+++ tests/testthat/test-DualSampler.R
@@ -13,4 +13,17 @@ if (torch::torch_is_installed() && requireNamespace("mlr3torch")) {
     expect_equal(dual_num_batches(4, 8, 2, 4), 2)
     expect_equal(dual_num_batches(2, 8, 2, 3), 2) # 2 neg sample unused
   })
+
+  test_that("test dual_class_indices", {
+    skip_on_cran()
+    labels <- c(0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0)
+    indices <- dual_class_indices(labels)
+    expect_equal(indices$pos, c(3, 6, 8, 11))
+    expect_equal(indices$neg, c(1, 2, 4, 5, 7, 9, 10, 12))
+    # must have both labels
+    expect_error(dual_class_indices(c(0, 0, 0, 0)))
+    expect_error(dual_class_indices(c(1, 1, 1)))
+    # don't support multiclass
+    expect_error(dual_class_indices(c(0, 1, 2)))
+  })
 }
