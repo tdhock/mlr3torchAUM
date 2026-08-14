1afd56a407a8599a0110031e9235c856add8d2d9
AmanKashyap0807 <akashyap_be23@thapar.edu>
2026-08-15 02:30:50 +0530

    test script

--- tests/testthat/test-Sampler.R
+++ tests/testthat/test-Sampler.R
@@ -8,5 +8,11 @@ test_that("test BaseSampler and check_sampling_strategy", {
   expect_equal(check_sampling_strategy(y, "auto", "under-sampling"), c("1" = 10L))
   expect_error(check_sampling_strategy(y, "unknown", "under-sampling"), "not implemented for under-sampling")
 
+  sampler <- BaseSampler$new()
+  expect_error(sampler$fit_resample(X, y), "not implemented")
+  sampler_under <- BaseSampler$new()
+  sampler_under$sampling_type <- "under-sampling"
+  expect_error(sampler_under$fit_resample(X, y), "not implemented")
+
   expect_error(BaseSampler$new()$fit_resample(matrix(rnorm(10), ncol = 2), y), "data and label not consistent")
 })
