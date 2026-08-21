cf00f43245687936e763cc86bca0be0829c60c66
AmanKashyap0807 <akashyap_be23@thapar.edu>
2026-08-21 12:53:15 +0530

    update test script to cover helper function

--- tests/testthat/test-Sampler.R
+++ tests/testthat/test-Sampler.R
@@ -16,3 +16,29 @@ test_that("test BaseSampler and check_sampling_strategy", {
 
   expect_error(BaseSampler$new()$fit_resample(matrix(rnorm(10), ncol = 2), y), "data and label not consistent")
 })
+
+test_that("test helper function count class", {
+  count_result <- count_class(factor(c(rep("0", 100), rep("1", 900))))
+  expect_identical(count_result, c("0" = 100L, "1" = 900L))
+})
+
+test_that("test auto mode of check_sampling strategy (multi-class)", {
+  samp_strategy_multi <- check_sampling_strategy(
+    factor(c(rep("0", 50), rep("1", 100), rep("2", 1000))),
+    strategy = "auto"
+  )
+  expect_identical(samp_strategy_multi, c("0" = 950L, "1" = 900L))
+})
+
+test_that("test minority mode of check_sampling strategy", {
+  samp_strategy_multi_mino <- check_sampling_strategy(
+    factor(c(rep("0", 50), rep("1", 100), rep("2", 1000))),
+    strategy = "minority"
+  )
+  expect_identical(samp_strategy_multi_mino, c("0" = 950L))
+  samp_strategy_multi_mino2 <- check_sampling_strategy(
+    factor(c(rep("0", 50), rep("1", 50), rep("2", 1000))),
+    strategy = "minority"
+  )
+  expect_identical(samp_strategy_multi_mino2, c("0" = 950L))
+})
