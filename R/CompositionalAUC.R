d126f01c592726df6f37d5247cd099dc4ef9856f
caowei <weicao92@163.com>
2026-09-03 15:42:32 -0400

    add learners in for scripts

--- R/CompositionalAUC.R
+++ R/CompositionalAUC.R
@@ -4,14 +4,19 @@ is_ce_step <- function(step, k) {
 
 nn_CompositionalAUC_loss <- torch::nn_module(
   c("nn_CompositionalAUC_loss", "nn_loss"),
-  initialize = function(margin = 1, k = 1, add_sigmoid = TRUE) {
+  initialize = function(
+    margin = 1, k = 1,
+    version = "v1", add_sigmoid = TRUE, imratio = NULL
+  ) {
     self$a <- torch::nn_parameter(torch::torch_zeros(1))
     self$b <- torch::nn_parameter(torch::torch_zeros(1))
     self$alpha <- torch::nn_parameter(torch::torch_zeros(1))
     self$step <- torch::nn_buffer(torch::torch_zeros(1))
     self$margin <- margin
     self$k <- k
+    self$version <- version
     self$add_sigmoid <- add_sigmoid
+    self$imratio <- imratio
   },
   forward = function(pred, target) {
     if (self$add_sigmoid) pred <- torch::torch_sigmoid(pred)
@@ -20,7 +25,7 @@ nn_CompositionalAUC_loss <- torch::nn_module(
         pred$flatten(), target$flatten()
       )
     } else {
-      loss <- AUCM(pred, target, self$a, self$b, self$alpha, self$margin)
+      loss <- AUCM(pred, target, self$a, self$b, self$alpha, self$margin, self$version, self$imratio)
     }
     self$step$add_(1L)
     return(loss)
